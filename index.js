56a85c0bd02340a23807ae57fbc427d7c5ba0c44
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-20 13:03:51 +0100

    Completed

--- index.js
+++ index.js
@@ -1,73 +1,31 @@
-const byronPoodle = {
-  name: "Byron",
-  ageInYears: 2,
-  warn: function() {
-    // console.log(`Bark bark bark`);
-    // console.log(this)
+const asgardianBrothers = [
+  {
+    firstName: "Thor",
+    familyName: "Odinsson"
+  },
+  {
+    firstName: "Loki",
+    familyName: "Laufeysson-Odinsson"
   }
-};
+]
 
+function intro(person, line) {
+  console.log( `${person.firstName} ${person.familyName} says: ${line}`
+)}
 
-byronPoodle.warn()
-
-//////////////////
-
-
-const blakeDoodle = {
-  name: "Blake",
-  breed: "Labradoodle",
-  sonicAttack: "ear-rupturing atomic bark",
-  mostHatedThing: "noises in the apartment hallway",
-  warn: function() {
-    // console.log(`${this.name} the ${this.breed} issues an ${this.sonicAttack} when he hears ${this.mostHatedThing}`);
-  }
-};
-
-blakeDoodle.warn();
+const phrase = "I like this brown drink very much, bring me another!"
+intro(asgardianBrothers[0], phrase)
 
 
 //////////////////////
 
-const frog = { name: "Kermit" };
-const pig = { name: "Miss Piggy" };
-const speak = function() { console.log( `It ain't easy being ${this.name}`)};
 
-frog.speak = speak;
-pig.speak = speak;
-// console.log(frog.speak === pig.speak);
-frog.speak()
-pig.speak()
+function introWithContext(line){
+  console.log( `${this.firstName} ${this.familyName} says: ${line}`
+)}
 
+introWithContext.call(asgardianBrothers[0], phrase)
+//=> Thor Odinsson says: I like this brown drink very much, bring me another!
 
-//////////////////////////////
-
-const contextReturner = function() {
-  return this;
-}
-
-// console.log(contextReturner());
-
-/////////////////////////
-
- class users {
-  constructor(fName, lName) 
-    {
-      this.firstName = fName;
-      this.lastName = lName;
-      this.status = "on";
-      this.game = "MAFIA 3";
-    }
-
-  displayer() {
-    console.log(`Hello, ${this.firstName + " " + this.lastName}. we are glad to tell you that your status is ${this.status} and you are doing goof playing ${this.game}`)
-  }
-  
-}
-
-
-const nUser = new users("Namo", "Bago")
-
-console.log(nUser)
-
-nUser.displayer()
-
+introWithContext.apply(asgardianBrothers[1], [phrase])
+//=> Thor Odinsson says: I like this brown drink very muc
